4fe56a14e651adda6e060ceae072a03056225b6f
Ayu Itz <77532758+ayush-py-dev@users.noreply.github.com>
2023-04-14 16:21:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ import config
 import asyncio
 from discord.ui import Button, button, View
 
+import chat_exporter
+from github import Github
+
+import time
+import os
+
 bot = commands.Bot(
     command_prefix="!",
     intents=discord.Intents.all(),
@@ -24,6 +30,38 @@ async def on_ready():
     bot.add_view(CloseButton())
     bot.add_view(TrashButton())
 
+
+# GET TRANSCRIPT
+async def get_transcript(member: discord.Member, channel: discord.TextChannel):
+    export = await chat_exporter.export(channel=channel)
+    file_name=f"{member.id}.html"
+    with open(file_name, "w", encoding="utf-8") as f:
+        f.write(export)
+
+# UPLOAD TO GITHUB
+def upload(file_path: str, member_name: str):
+    github = Github(config.GTOKEN)
+    repo = github.get_repo("ayush-py-dev/ticket")
+    file_name = f"{int(time.time())}"
+    repo.create_file(
+        path=f"tickets/{file_name}.html",
+        message="Ticket Log for {0}".format(member_name),
+        branch="main",
+        content=open(f"{file_path}","r",encoding="utf-8").read()
+    )
+    os.remove(file_path)
+
+    return file_name
+
+
+async def send_log(title: str, guild: discord.Guild, description: str, color: discord.Color):
+    log_channel = guild.get_channel(1094833261198458951)
+    embed = discord.Embed(
+        title=title,
+        description=description,
+        color=color
+    )
+    await log_channel.send(embed=embed)
 
 class CreateButton(View):
     def __init__(self):
@@ -66,6 +104,14 @@ class CreateButton(View):
             ephemeral=True
         )
 
+        await send_log(
+            title="Ticekt Created",
+            description="Created by {0}".format(interaction.user.mention),
+            color=discord.Color.green(),
+            guild=interaction.guild
+        )
+        
+
 
 class CloseButton(View):
     def __init__(self):
@@ -94,6 +140,16 @@ class CloseButton(View):
             ),
             view = TrashButton()
         )
+        member = interaction.guild.get_member(int(interaction.channel.topic.split(" ")[0]))
+        await get_transcript(member=member, channel=interaction.channel)
+        file_name = upload(f'{member.id}.html',member.name)
+        link = f"https://ayush-py-dev.github.io/ticket/tickets/{file_name}"
+        await send_log(
+            title="Ticket Closed",
+            description=f"Closed by: {interaction.user.mention}\n[click for transcript]({link})",
+            color=discord.Color.yellow(),
+            guild=interaction.guild
+        )
     
 
 class TrashButton(View):
@@ -107,10 +163,13 @@ class TrashButton(View):
         await asyncio.sleep(3)
 
         await interaction.channel.delete()
+        await send_log(
+            title="Ticket Deleted",
+            description=f"Deleted by {interaction.user.mention}, ticket: {interaction.channel.name}",
+            color=discord.Color.red(),
+            guild=interaction.guild
+        )
         
-
-
-
 
 @bot.command(name="ticket")
 @commands.has_permissions(administrator=True)
